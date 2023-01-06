ffa9988399e7e6e5635051e187ab45abec09042d
marie taylor <marietaylortailor@hotmail.com>
2023-01-06 21:54:15 +0000

    Refactored-list-with-array-&-iteration

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 #first we print the list of students
 students = [
 "The students of Villains Academy",
@@ -18,15 +17,8 @@ students = [
 # and then print them
 puts "The students of Villains Academy"
 puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts "Overall puts students, we have #{student_count} great students"
+students.each do |student|
+  puts students
+end
+#finally, we print the total
+puts "Overall puts students, we have #{student.count} great students"
